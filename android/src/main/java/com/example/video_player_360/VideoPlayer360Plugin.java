1219be68913165514f106980ad6506455054be78
devanshramen <devansh.ramen@telecom.mu>
2020-01-15 15:42:43 +0400

    Added support Android

--- android/src/main/java/com/example/video_player_360/VideoPlayer360Plugin.java
+++ android/src/main/java/com/example/video_player_360/VideoPlayer360Plugin.java
@@ -1,6 +1,13 @@
 package com.example.video_player_360;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.annotation.NonNull;
+
+import com.example.video_player_360.videoplayer.VideoActivity;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -10,10 +17,22 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** VideoPlayer360Plugin */
 public class VideoPlayer360Plugin implements FlutterPlugin, MethodCallHandler {
+  private Context context;
+  public static String urlVideo;
+
+
+  public VideoPlayer360Plugin() {
+
+  }
+
+  public VideoPlayer360Plugin(Context context) {
+    this.context = context;
+  }
+
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "video_player_360");
-    channel.setMethodCallHandler(new VideoPlayer360Plugin());
+    final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "innov.lab/video_player_360");
+    channel.setMethodCallHandler(new VideoPlayer360Plugin(flutterPluginBinding.getApplicationContext()));
   }
 
   // This static function is optional and equivalent to onAttachedToEngine. It supports the old
@@ -26,18 +45,26 @@ public class VideoPlayer360Plugin implements FlutterPlugin, MethodCallHandler {
   // depending on the user's project. onAttachedToEngine or registerWith must both be defined
   // in the same class.
   public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "video_player_360");
-    channel.setMethodCallHandler(new VideoPlayer360Plugin());
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "innov.lab/video_player_360");
+    channel.setMethodCallHandler(new VideoPlayer360Plugin(registrar.context()));
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
-    }
-  }
+
+      if (call.method.equals("playvideo")) {
+
+          urlVideo = call.argument("video_url");
+
+          Intent i = new Intent(context, VideoActivity.class);
+          i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+          context.startActivity(i);
+          result.success("loading video");
+      } else {
+          result.notImplemented();
+      }
+   }
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
